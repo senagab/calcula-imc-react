d5561da74266942f90e4f7a9263d8be5a09cd169
senagab <senagab@gmail.com>
2024-12-30 23:06:20 -0400

    feat: calculo imc logica

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ function App() {
 
   return (
     <>
+      <header>
+        <h1>
+          Para calcular seu IMC informe altura e peso abaixo:
+        </h1>
+      </header>
       <div className='form'>
         <div className='container-input'>
           <input className='input' type="number" placeholder='altura' />
@@ -19,6 +24,7 @@ function App() {
       <div className='container-resultado'>
         <span className='resultado'>resultado</span>
       </div>
+      {/* <Calculo altura={altura} peso={peso} ></Calculo> */}
     </>
   )
 }
