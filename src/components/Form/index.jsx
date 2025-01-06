1093d0e6cc8332a32d8e4a7397918a3ae8a72eff
senagab <senagab@gmail.com>
2025-01-06 19:47:20 -0400

    chore: remoção de código + feat: icone-info no estilo global

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -29,7 +29,7 @@ const IMC = ({ altura, peso }) => {
     const tabelaImc = [
         { limite: 18.5, classificacao: `Abaixo do peso`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à um índice abaixo do peso ideal, levando em conta sua altura.`, cor: styles.baixoPeso, detalhe: `até 18.5` },
         { limite: 24.9, classificacao: `Peso normal`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.pesoNormal, detalhe: `até 24.9` },
-        { limite: 29.9, classificacao: `Sobrepeso`, conteudo: `cSegundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`, cor: styles.sobrepeso, detalhe: `até 29.9` },
+        { limite: 29.9, classificacao: `Sobrepeso`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`, cor: styles.sobrepeso, detalhe: `até 29.9` },
         { limite: 34.9, classificacao: `Obesidade grau I.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeI, detalhe: `até 34.9` },
         { limite: 39.9, classificacao: `Obesidade grau II.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeII, detalhe: `até 39.9` },
         { limite: 40, classificacao: `Obesidade grau III.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeIII, detalhe: `acima de 40` },
@@ -54,9 +54,9 @@ const IMC = ({ altura, peso }) => {
                             <div className={classificacaoIMC.cor}>
                                 <p className='titulo'>{classificacaoIMC.classificacao}</p>
                                 <span className='subtitulo'>{classificacaoIMC.detalhe}</span>
-                                <div className='icone-info'>ico</div>
-                                <p>
-                                    Segundo a OMS você está {classificacaoIMC.conteudo}
+                                <div className='icone-info'></div>
+                                <p className='detalhe'>
+                                    {classificacaoIMC.conteudo}
                                 </p>
                             </div>
                         )}
