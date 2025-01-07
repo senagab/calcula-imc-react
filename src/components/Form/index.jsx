ee8029d2be13352eab5990f7709d74235cba3744
senagab <senagab@gmail.com>
2025-01-07 18:19:36 -0400

    feat: alteração de icone info e estilização + card tamanho + texto tamanho

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -27,12 +27,36 @@ const IMC = ({ altura, peso }) => {
     };
 
     const tabelaImc = [
-        { limite: 18.5, classificacao: `Abaixo do peso`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à um índice abaixo do peso ideal, levando em conta sua altura.`, cor: styles.baixoPeso, detalhe: `até 18.5` },
-        { limite: 24.9, classificacao: `Peso normal`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.pesoNormal, detalhe: `até 24.9` },
-        { limite: 29.9, classificacao: `Sobrepeso`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`, cor: styles.sobrepeso, detalhe: `até 29.9` },
-        { limite: 34.9, classificacao: `Obesidade grau I.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeI, detalhe: `até 34.9` },
-        { limite: 39.9, classificacao: `Obesidade grau II.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeII, detalhe: `até 39.9` },
-        { limite: 40, classificacao: `Obesidade grau III.`, conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,cor: styles.obesidadeIII, detalhe: `acima de 40` },
+        { 
+            limite: 18.5, classificacao: `Abaixo do peso`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à um índice abaixo do peso ideal, levando em conta sua altura.`,
+            cor: styles.baixoPeso, detalhe: `até 18.5` 
+        },
+        { 
+            limite: 24.9, classificacao: `Peso normal`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,
+            cor: styles.pesoNormal, detalhe: `até 24.9` 
+        },
+        { 
+            limite: 29.9, classificacao: `Sobrepeso`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,
+            cor: styles.sobrepeso, detalhe: `até 29.9` 
+        },
+        { 
+            limite: 34.9, classificacao: `Obesidade grau I.`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,
+            cor: styles.obesidadeI, detalhe: `até 34.9` 
+        },
+        { 
+            limite: 39.9, classificacao: `Obesidade grau II.`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,
+            cor: styles.obesidadeII, detalhe: `até 39.9` 
+        },
+        {   
+            limite: 40, classificacao: `Obesidade grau III.`,
+            conteudo: `Segundo a Organização Mundial da Saúde (OMS), esta pontuação equivale à umlevando em conta sua altura.`,
+            cor: styles.obesidadeIII, detalhe: `acima de 40` 
+        },
     ];
 
     const erro = verificaCampos();
@@ -51,13 +75,15 @@ const IMC = ({ altura, peso }) => {
                     <>
                         <h2>Seu IMC é: {imc}</h2>
                         {classificacaoIMC && (
-                            <div className={classificacaoIMC.cor}>
-                                <p className='titulo'>{classificacaoIMC.classificacao}</p>
-                                <span className='subtitulo'>{classificacaoIMC.detalhe}</span>
-                                <div className='icone-info'></div>
-                                <p className='detalhe'>
-                                    {classificacaoIMC.conteudo}
-                                </p>
+                            <div className='container-imc'>
+                                <div className={classificacaoIMC.cor}>
+                                    <p className='titulo'>{classificacaoIMC.classificacao}</p>
+                                    <span className='subtitulo'>{classificacaoIMC.detalhe}</span>
+                                    <div className='icone-info'></div>
+                                    <p className='detalhe'>
+                                        {classificacaoIMC.conteudo}
+                                    </p>
+                                </div>
                             </div>
                         )}
                     </>
